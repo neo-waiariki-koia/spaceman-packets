aee4fc6af0fa65ccfca6e806de527899dbb2578e
neo-waiariki-koia <waiariki.koia@neotechnology.com>
2023-02-23 15:53:51 +0000

    Initial commit

---
+++ pkg/packet/ip.go
@@ -0,0 +1,61 @@
+package packet
+
+import (
+	"bytes"
+	"encoding/binary"
+)
+
+type IPHeader struct {
+	Version            uint8
+	Protocol           uint8
+	SourceAddress      uint32
+	sAddr              string
+	DestinationAddress uint32
+	dAddr              string
+	Data               []byte
+}
+
+func UnmarshalIP(data []byte) *IPHeader {
+	var ipHeader IPHeader
+
+	firstSection := binary.BigEndian.Uint16(data[0:2])
+	ipHeader.Version = byte(firstSection >> 12) // top 4 bits
+
+	thirdSection := binary.BigEndian.Uint16(data[8:10])
+	_ = byte(thirdSection >> 8)                 // top 8 bits
+	ipHeader.Protocol = byte(thirdSection >> 0) // second 8 bits
+
+	ipHeader.SourceAddress = binary.BigEndian.Uint32(data[12:16])
+	ipHeader.DestinationAddress = binary.BigEndian.Uint32(data[16:20])
+
+	ipHeader.Data = data[20:]
+
+	return &ipHeader
+}
+
+func (ih *IPHeader) validateTcpPacket() bool {
+	return ih.Protocol == 6
+}
+
+func (ih *IPHeader) MarshalIP() []byte {
+	buf := new(bytes.Buffer)
+
+	binary.Write(buf, binary.BigEndian, []byte{69, 0, 0, 40})   // Version, IHL, Type of Service | Total Length
+	binary.Write(buf, binary.BigEndian, []byte{141, 245, 0, 0}) // Identification | Flags, Fragment Offset
+	binary.Write(buf, binary.BigEndian, []byte{64, 6, 0, 0})    // TTL, Protocol | Header Checksum
+	binary.Write(buf, binary.BigEndian, []byte{192, 168, 1, 101})
+	binary.Write(buf, binary.BigEndian, []byte{192, 168, 1, 100})
+
+	bufBytes := buf.Bytes()
+
+	checksum := calculateChecksum(bufBytes)
+
+	buf2 := new(bytes.Buffer)
+	binary.Write(buf2, binary.BigEndian, checksum)
+
+	ipHeader := bufBytes[:10]
+	ipHeader = append(ipHeader, buf2.Bytes()...)
+	ipHeader = append(ipHeader, bufBytes[12:]...)
+
+	return ipHeader
+}
---
+++ pkg/packet/nonsocket.go
@@ -0,0 +1,123 @@
+package packet
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"sync"
+	"time"
+)
+
+func (ip *IPPacket) createPacket() []byte {
+	ack := 0
+	seq := 0
+	flags := []string{"syn"}
+	data := make([]byte, 0)
+	packet := ip.buildTCPPacket(uint32(seq), uint32(ack), flags, data)
+	return packet
+}
+
+func (ip *IPPacket) buildTCPPacket(seq, ack uint32, flags []string, data []byte) []byte {
+	tcpHeader := ip.TCPHeader.MarshalTCP(seq, ack, flags)
+
+	return tcpHeader
+}
+
+func SendPacketNoSocket(remoteHost string, destPort uint16, data []byte) {
+	localAddr := interfaceAddress("en0")
+	laddr := strings.Split(localAddr.String(), "/")[0] // Clean addresses like 192.168.1.30/24
+
+	ipPacket := NewIPPacket(remoteHost, destPort, laddr, 25566, data)
+
+	packet := ipPacket.createPacket()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	var receiveTime time.Time
+
+	addrs, err := net.LookupHost(remoteHost)
+	if err != nil {
+		log.Fatalf("Error resolving %s. %s\n", remoteHost, err)
+	}
+	remoteAddr := addrs[0]
+
+	go func() {
+		receiveTime = receiveSynAck(laddr, remoteAddr)
+		wg.Done()
+	}()
+
+	time.Sleep(1 * time.Millisecond)
+
+	sendTime := sendSyn(remoteAddr, destPort, packet)
+
+	wg.Wait()
+
+	fmt.Println(receiveTime.Sub(sendTime))
+}
+
+func sendSyn(raddr string, port uint16, packet []byte) time.Time {
+	conn, err := net.Dial("ip4:tcp", raddr)
+	if err != nil {
+		log.Fatalf("Dial: %s\n", err)
+	}
+
+	sendTime := time.Now()
+
+	numWrote, err := conn.Write(packet)
+	if err != nil {
+		log.Fatalf("Write: %s\n", err)
+	}
+	if numWrote != len(packet) {
+		log.Fatalf("Short write. Wrote %d/%d bytes\n", numWrote, len(packet))
+	}
+
+	err = conn.Close()
+	if err != nil {
+		log.Fatalf("Close: %s\n", err)
+	}
+
+	return sendTime
+}
+
+func receiveSynAck(localAddress, remoteAddress string) time.Time {
+	netaddr, err := net.ResolveIPAddr("ip4", localAddress)
+	if err != nil {
+		log.Fatalf("net.ResolveIPAddr: %s. %s\n", localAddress, netaddr)
+	}
+
+	conn, err := net.ListenIP("ip4:tcp", netaddr)
+	if err != nil {
+		log.Fatalf("ListenIP: %s\n", err)
+	}
+
+	var receiveTime time.Time
+	for {
+		buf := make([]byte, 1024)
+		_, raddr, err := conn.ReadFrom(buf)
+		if err != nil {
+			log.Fatalf("ReadFrom: %s\n", err)
+		}
+
+		fmt.Println("HEREo")
+		if raddr.String() != remoteAddress {
+			fmt.Println("HERE1")
+			// this is not the packet we are looking for
+			continue
+		}
+
+		fmt.Println("HERE2")
+		ipData := buf[14:]
+		ipHeader := UnmarshalIP(ipData)
+		fmt.Printf("%v\n", ipHeader)
+		if validPacket := ipHeader.validateTcpPacket(); !validPacket {
+			log.Fatal("Not a valid TCP Packet")
+		}
+
+		tcpHeader := UnmarshalTCP(ipHeader.Data)
+		fmt.Printf("%v\n", tcpHeader)
+		break
+	}
+
+	return receiveTime
+}
