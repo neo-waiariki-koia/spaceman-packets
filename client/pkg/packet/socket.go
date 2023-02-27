8363cb56cd1606b38a1d82391c7a7781ba858f21
neo-waiariki-koia <waiariki.koia@neotechnology.com>
2023-02-27 12:50:55 +0000

    Add socket code

---
+++ client/pkg/packet/socket.go
@@ -0,0 +1,68 @@
+package packet
+
+import (
+	"fmt"
+	"log"
+	"syscall"
+	"time"
+)
+
+func SendPacketSocket(remoteHost string, destPort, srcPort uint16) {
+	laddr := "127.0.0.1"
+	ipPacket := NewIPPacket(remoteHost, destPort, laddr, srcPort, []byte{})
+
+	packet := ipPacket.createPacket()
+
+	fmt.Println("SENDING")
+	sendTime := socketSendSyn(remoteHost, destPort, packet)
+	fmt.Println("RECEIVING")
+	receiveTime := socketReceiveSynAck(laddr, int(srcPort))
+
+	fmt.Println(receiveTime.Sub(sendTime))
+}
+
+func socketSendSyn(raddr string, port uint16, packet []byte) time.Time {
+	fd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_RAW, syscall.IPPROTO_TCP)
+	if err != nil {
+		log.Fatalf("Socket: %s\n", err)
+	}
+
+	addr := syscall.SockaddrInet4{
+		Addr: to4byte(raddr),
+		Port: int(port),
+	}
+
+	fmt.Printf("Sending to %s:%d\n", raddr, port)
+
+	err = syscall.Sendto(fd, packet, 0, &addr)
+	if err != nil {
+		log.Fatal("Sendto:", err)
+	}
+
+	return time.Now()
+}
+
+func socketReceiveSynAck(localAddress string, localPort int) time.Time {
+	fd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_RAW, syscall.IPPROTO_TCP)
+	if err != nil {
+		log.Fatal("Socket: ", err)
+	}
+
+	buf := make([]byte, 1024)
+	numRead, _, err := syscall.Recvfrom(fd, buf, 0)
+	if err != nil {
+		log.Fatal("Recvfrom: ", err)
+	}
+	fmt.Printf("% X\n", buf[:numRead])
+
+	ipData := buf[14:]
+	ipHeader := UnmarshalIP(ipData)
+	if validPacket := ipHeader.validateTcpPacket(); !validPacket {
+		log.Fatal("Not a valid TCP Packet")
+	}
+
+	tcpHeader := UnmarshalTCP(ipHeader.Data)
+	fmt.Printf("%v\n", tcpHeader)
+
+	return time.Now()
+}
